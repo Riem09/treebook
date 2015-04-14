621fbbaa779dde0524747b75b9e73cece635c661
Matthew Riemer <mattriemer36@gmail.com>
2015-04-14 12:47:39 +0200

    Updated frontend for the new authentication system
    Added simple_form
    Updated several forms to bootstrap/simple_form
    Added login/logout/register/profile links to nav
    Added a collection select to our status form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+has_many :statuses
+
+def full_name
+
+first_name + " " + last_name
+
+end
+
+
 
         #attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
 end
--- db/migrate/20150412191711_devise_create_users.rb
+++ db/migrate/20150412191711_devise_create_users.rb
@@ -39,7 +39,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email,                unique: true
+    add_index :first_name, :last_name, :profile_name
     add_index :users, :reset_password_token, unique: true
+
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
